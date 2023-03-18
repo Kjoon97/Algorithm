a56f4b7c4b1f14998561201347b3ff777974d4fc
joonhyuck <jh83370@naver.com>
2023-03-19 00:28:28 +0900

    boj-1026-보물

---
+++ boj/그리디/보물.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("input.txt","r")
+
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+a.sort()
+b.sort(reverse=True)
+s =0
+for i in range(n):
+    s+=a[i]*b[i]
+print(s)
